936f6d2ff7e6e185b35b9c3d9570339b49404a02
Jim Jiracek <james.jiracek@slalom.com>
2025-12-11 09:37:49 -0500

    section 17: splash screen

--- App.tsx
+++ App.tsx
@@ -10,8 +10,23 @@ import {
   SafeAreaProvider,
 } from 'react-native-safe-area-context';
 import CameraGallery from './src/lessons/CameraGallery';
+import BootSplash from "react-native-bootsplash";
+import { useEffect } from 'react';
+
 
 function App() {
+
+  // This is from bootsplash. Hide the splash screen once the app is ready
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({ fade: true });
+      console.log("BootSplash has been hidden successfully");
+    });
+  }, []);
 
   return (
     <SafeAreaProvider>
---
+++ splash.md
@@ -0,0 +1,7 @@
+yarn react-native-bootsplash generate src/assets/images/splash.png \
+  --platforms=android,ios,web \
+  --background=000000 \
+  --logo-width=120 \
+  --assets-output=assets/bootsplash \
+  --flavor=main \
+  --html=public/index.html
